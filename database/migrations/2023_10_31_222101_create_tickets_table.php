73a7feb6cc22ebb573c5c26dbb85660423a0508d
ericdourado <ericdourado1@hotmail.com>
2023-10-31 19:37:02 -0300

    criado estrutura de banco e sistema

--- database/migrations/2023_10_31_222101_create_tickets_table.php
+++ database/migrations/2023_10_31_222101_create_tickets_table.php
@@ -11,16 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tickets', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('gender')->nullable();
-            $table->string('role_id');
-            $table->rememberToken();
+            $table->integer('user_id');
+            $table->integer('problem_id');
+            $table->integer('suport_id')->nullable();
+            $table->string('ticket_name');
+            $table->string('anydesk')->nullable();
+            $table->string('situation');
+            $table->boolean('suport_started');
             $table->timestamps();
+
         });
     }
 
@@ -29,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tickets');
     }
 };
--- database/migrations/2023_10_31_222126_create_problems_table.php
+++ database/migrations/2023_10_31_222126_create_problems_table.php
@@ -11,15 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('problem', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('gender')->nullable();
-            $table->string('role_id');
-            $table->rememberToken();
+            $table->string('description');
             $table->timestamps();
         });
     }
@@ -29,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('problems');
     }
 };
