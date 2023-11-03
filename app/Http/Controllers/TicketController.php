cb32affcab497fe82ed3686fcb14e6937dd4e9a7
leonardogiagio <leonardogiagio@gmail.com>
2023-11-02 22:30:39 -0300

    add: lista de tickets na view

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -13,26 +13,34 @@ class TicketController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request )
+    public function index(Request $request)
     {
         // $tickets = new ticket();
         // $tickets = $tickets->get();
 
         $tickets = DB::table('tickets')
-                        ->join('users as cliente', 'cliente.id', '=', 'tickets.user_id' )
-                        ->join('users as tecnico', 'tecnico.id', '=', 'tickets.suport_id')
-                        ->select('cliente.id as id_cliente', 'cliente.name as nome_cliente', 'tecnico.id as tecnico_id', 
-                                'tecnico.name as tecnico.name', 'tickets.*')
-                        ->get();
+            ->join('users as cliente', 'cliente.id', '=', 'tickets.user_id')
+            ->leftjoin('users as tecnico', 'tecnico.id', '=', 'tickets.suport_id')
+            ->select(
+                'cliente.id as id_cliente',
+                'cliente.name as cliente_nome',
+                'tecnico.id as tecnico_id',
+                'tecnico.name as tecnico_nome',
+                'tickets.id as ticket_id',
+                'tickets.ticket_name as ticket_nome',
+                'tickets.description as ticket_descricao',
+                'tickets.created_at as ticket_criado_em',
+                'tickets.updated_at as ticket_atualizado_em'
+            )
+            ->get();
 
         return view('tickets.listar-tickets')->with('tickets', $tickets);
-
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create(Request $request )
+    public function create(Request $request)
     {
         return view('tickets.criar-novo-ticket');
     }
@@ -52,7 +60,6 @@ class TicketController extends Controller
      */
     public function show(ticket $ticket)
     {
-        
     }
 
     /**
--- resources/views/tickets/listar-tickets.blade.php
+++ resources/views/tickets/listar-tickets.blade.php
@@ -2,7 +2,6 @@
     .fundo-laranja {
         background-color: #FF8C00;
     }
-    
 </style>
 
 <x-app-layout>
@@ -17,10 +16,36 @@
             <div class="fundo-laranja overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
 
+                    <table class="table w-full">
+                        <thead>
+                            <tr>
+                                <th class="text-left px-4 py-2">ID</th>
+                                <th class="text-left px-4 py-2">Título</th>
+                                <th class="text-left px-4 py-2">Descrição</th>
+                                <th class="text-left px-4 py-2">Criador</th>
+                                <th class="text-left px-4 py-2">Técnico</th>
+                                <th class="text-left px-4 py-2">Criado em</th>
+                                <th class="text-left px-4 py-2">Última atualização</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($tickets as $ticket)
+                                @php $ticket->ticket_criado_em = date('d/m/Y', strtotime($ticket->ticket_criado_em)); @endphp
+                                @php $ticket->ticket_atualizado_em = date('d/m/Y', strtotime($ticket->ticket_atualizado_em)); @endphp
+                                <tr>
+                                    <td class="px-4 py-2 text-white">{{ $ticket->ticket_id }}</td>
+                                    <td class="px-4 py-2 text-white">{{ $ticket->ticket_nome }}</td>
+                                    <td class="px-4 py-2 text-white" style="width: 20px; white-space: nowrap; text-overflow: ellipsis;">{{ $ticket->ticket_descricao }}</td>
+                                    <td class="px-4 py-2 text-white">{{ $ticket->cliente_nome }}</td>
+                                    <td class="px-4 py-2 text-white">{{ $ticket->tecnico_nome }}</td>
+                                    <td class="px-4 py-2 text-white">{{ $ticket->ticket_criado_em }}</td>
+                                    <td class="px-4 py-2 text-white">{{ $ticket->ticket_atualizado_em }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
 
-                    listar tickets
 
-                    
                 </div>
             </div>
         </div>
