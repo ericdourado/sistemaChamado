9755dc039cde625876f18a75b038f2a1c9d00df7
leonardogiagio <leonardogiagio@gmail.com>
2023-11-07 14:43:04 -0300

    add: link no id para editar ticket

--- resources/views/tickets/listar-tickets.blade.php
+++ resources/views/tickets/listar-tickets.blade.php
@@ -116,7 +116,7 @@
                                 class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                 <td scope="row"
                                     class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    {{ $ticket->ticket_id }}</td>
+                                    <a href="/editar-ticket/{{ $ticket->ticket_id }}">{{ $ticket->ticket_id }}</a></td>
                                 <td scope="row"
                                     class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                     {{ $ticket->ticket_nome }}</td>
